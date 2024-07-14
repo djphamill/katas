4e1cdc7d99bc85dc78dabb1744952e64befb93ae
david <djphamill@gmail.com>
2024-07-14 13:30:57 +0100

    Ensure that non-trival height arrays can be solved.

--- codility/lessons/07/stonewall/solution_test.py
+++ codility/lessons/07/stonewall/solution_test.py
@@ -4,6 +4,10 @@ from solution import solution
 TEST_CASES = [
     ("uniform wall of length one", [1], 1),
     ("uniform wall of length four", [3,3,3,3], 1),
+    ("example provided", [8, 8, 5, 7, 9, 8, 7, 4, 8], 7),
+    ("steps of unit length", [1, 2, 3, 4, 5], 5),
+    ("steps of length two", [1, 1, 2, 2, 3, 3, 4, 4, 5, 5], 5),
+    ("triangle", [1, 3, 5, 3, 1], 3),
 ]
 
 @parameterized.expand(TEST_CASES)
