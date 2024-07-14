d07ddcf41f54313c0580195dda9f47bbe3f25eaf
david <djphamill@gmail.com>
2024-07-14 15:05:04 +0100

    Rename vairable to better reflect its purpose/meaning.

--- codility/lessons/06/numberofdiscintersections/solution_test.py
+++ codility/lessons/06/numberofdiscintersections/solution_test.py
@@ -14,7 +14,7 @@ def test_solution():
     (1, 0, [], [[0], [0]]),
     (1, 0, [[2]], [[2, 0], [0]]),
 ])
-def test_populate_points_with_disc_numbers(radius, position, current_coverage, expected):
-    actual = populate_points_with_disc_numbers(radius, position, current_coverage)
+def test_populate_points_with_disc_numbers(radius, disc_number, current_coverage, expected):
+    actual = populate_points_with_disc_numbers(radius, disc_number, current_coverage)
     assert actual == expected
 
