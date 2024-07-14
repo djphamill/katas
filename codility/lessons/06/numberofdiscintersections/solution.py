dcff429a0dc4b98f03f2850fb832b49c3190017f
david <djphamill@gmail.com>
2024-07-14 15:48:03 +0100

    Run through each disc; add it to the point's list of discs covering that point; run through all pairwise combindations of discs that are on a point, using sets to stop duplications.

--- codility/lessons/06/numberofdiscintersections/solution.py
+++ codility/lessons/06/numberofdiscintersections/solution.py
@@ -1,14 +1,24 @@
 from typing import List
+from itertools import combinations
 
 def solution(radii: List[int]):
-    pass
+    discs_on_points: List[List[int]] = []
+    for disc_number, radius in enumerate(radii):
+        discs_on_points = populate_points_with_disc_numbers(radius, disc_number, discs_on_points)
 
-def populate_points_with_disc_numbers(radius: int, disc_number: int, current_coverage: List[List[int]]) -> List[List[int]]:
+    pairings = set()
+    for discs_on_point in discs_on_points:
+        for pairing in combinations(discs_on_point, r=2):
+            pairings.add(pairing)
+
+    return len(pairings)
+
+def populate_points_with_disc_numbers(radius: int, disc_number: int, discs_on_points: List[List[int]]) -> List[List[int]]:
     start_postion_to_populate = max(disc_number - radius, 0)
     end_position_to_populate = disc_number + radius
     for position in range(start_postion_to_populate, end_position_to_populate + 1):
-        if len(current_coverage) < position + 1:
-            current_coverage.append([])
-        current_coverage[position].append(disc_number)
-    return current_coverage
+        if len(discs_on_points) < position + 1:
+            discs_on_points.append([])
+        discs_on_points[position].append(disc_number)
+    return discs_on_points
 
