f63bb5f5f38b432487feeafa35ca80f5465a0499
david <djphamill@gmail.com>
2024-07-14 14:49:02 +0100

    Ensure that the list populate is populating correctly.

---
+++ codility/lessons/06/numberofdiscintersections/solution_test.py
@@ -0,0 +1,20 @@
+from solution import solution, populate_points_with_disc_numbers
+
+from parameterized import parameterized
+
+def test_solution():
+    pass
+
+@parameterized.expand([
+    (0, 2, [[], [], [], []], [[], [], [2], []]),
+    (2, 2, [[], [], [], [], []], [[2], [2], [2], [2], [2]]),
+    (2, 0, [[], [], [], [], []], [[0], [0], [0], [], []]),
+    (1, 0, [[], [], [], [], []], [[0], [0], [], [], []]),
+    (1, 0, [[], [], [], [], []], [[0], [0], [], [], []]),
+    (1, 0, [], [[0], [0]]),
+    (1, 0, [[2]], [[2, 0], [0]]),
+])
+def test_populate_points_with_disc_numbers(radius, position, current_coverage, expected):
+    actual = populate_points_with_disc_numbers(radius, position, current_coverage)
+    assert actual == expected
+
