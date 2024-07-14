b5979df5e56771218335380ca32127d5deafb26d
david <djphamill@gmail.com>
2024-07-14 13:19:08 +0100

    Add test for wall of length one.

--- codility/lessons/07/stonewall/solution_test.py
+++ codility/lessons/07/stonewall/solution_test.py
@@ -2,7 +2,8 @@ from parameterized import parameterized
 from solution import solution
 
 TEST_CASES = [
-    ("uniform wall", [1,1,1,1], 1),
+    ("uniform wall of length one", [1], 1),
+    ("uniform wall of length four", [3,3,3,3], 1),
 ]
 
 @parameterized.expand(TEST_CASES)
