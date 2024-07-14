aa1535b23935697034f762d683c1d856485a72de
david <djphamill@gmail.com>
2024-07-14 15:15:24 +0100

    Add some test cases.

--- codility/lessons/06/numberofdiscintersections/solution_test.py
+++ codility/lessons/06/numberofdiscintersections/solution_test.py
@@ -2,8 +2,16 @@ from solution import solution, populate_points_with_disc_numbers
 
 from parameterized import parameterized
 
-def test_solution():
-    pass
+@parameterized.expand([
+    ("example provided", [1, 5, 2, 1, 4, 0], 11),
+    ("trivial", [4], 0),
+    ("none overlap", [0, 0, 0, 0], 0),
+    ("kissing only", [1, 0], 1),
+    ("only one pair, but lots", [99, 100], 1)
+])
+def test_solution(_, radii, expected):
+    actual = solution(radii)
+    assert actual == expected
 
 @parameterized.expand([
     (0, 2, [[], [], [], []], [[], [], [2], []]),
