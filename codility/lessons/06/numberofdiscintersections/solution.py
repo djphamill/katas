884e8bd78d823d264195ea92c707cc69b7b88515
David <djphamill@gmail.com>
2024-08-03 21:45:35 +0100

    Apply line length limit (it would appear).

--- codility/lessons/06/numberofdiscintersections/solution.py
+++ codility/lessons/06/numberofdiscintersections/solution.py
@@ -1,10 +1,12 @@
 from typing import List
 from itertools import combinations
 
+
 def solution(radii: List[int]):
     discs_on_points: List[List[int]] = []
     for disc_number, radius in enumerate(radii):
-        discs_on_points = populate_points_with_disc_numbers(radius, disc_number, discs_on_points)
+        discs_on_points = populate_points_with_disc_numbers(
+            radius, disc_number, discs_on_points)
 
     pairings = set()
     for discs_on_point in discs_on_points:
@@ -13,7 +15,10 @@ def solution(radii: List[int]):
 
     return len(pairings)
 
-def populate_points_with_disc_numbers(radius: int, disc_number: int, discs_on_points: List[List[int]]) -> List[List[int]]:
+
+def populate_points_with_disc_numbers(radius: int,
+                                      disc_number: int,
+                                      discs_on_points: List[List[int]]) -> List[List[int]]:
     start_postion_to_populate = max(disc_number - radius, 0)
     end_position_to_populate = disc_number + radius
     for position in range(start_postion_to_populate, end_position_to_populate + 1):
@@ -21,4 +26,3 @@ def populate_points_with_disc_numbers(radius: int, disc_number: int, discs_on_po
             discs_on_points.append([])
         discs_on_points[position].append(disc_number)
     return discs_on_points
-
