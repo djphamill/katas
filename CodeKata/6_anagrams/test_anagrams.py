195cba1bb2218ed6f440ddafbfb6ae34b9b2c48b
david <djphamill@gmail.com>
2024-08-04 20:21:20 +0100

    Misunderstood the brief.

--- CodeKata/6_anagrams/test_anagrams.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from anagrams import find_anagrams
-
-
-def test_find_anagrams():
-    actual = find_anagrams('dog')
-    expected = ['dog', 'god']
-    assert sorted(actual) == sorted(expected)
